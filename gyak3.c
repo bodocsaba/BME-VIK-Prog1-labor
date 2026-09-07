f92cc7581535d5858f9de1e22caacd7336d63de4
Bodó Csaba <115866118+bodocsaba@users.noreply.github.com>
2026-09-07 20:24:10 +0200

    Add files via upload

---
+++ gyak3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    printf("Prim-e?\n");
+    printf("Adj meg egy szamot: ");
+    int szam;
+    int prim = 1;
+    scanf("%d",&szam);
+    for(int i = 2;i < szam;i=i+1){
+        if(szam % i == 0){
+            prim = 0;
+            break;
+        }
+        else{
+            prim = 1;
+        }
+    }
+    if(prim == 1){
+        printf("A szam: %d prim.",szam);
+    }
+    else{
+        printf("A szam: %d nem prim.",szam);
+    }
+    return 0;
+}
